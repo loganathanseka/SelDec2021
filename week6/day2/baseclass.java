532c2d16af84ed81723a1c6476f4a23461599b2c
loganathanseka <96675817+loganathanseka@users.noreply.github.com>
2022-02-05 08:57:05 +0530

    Add files via upload

---
+++ week6/day2/baseclass.java
@@ -0,0 +1,32 @@
+package base_feature;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class baseclass extends AbstractTestNGCucumberTests {
+	public static ChromeDriver driver;
+
+	@BeforeMethod
+	  public void beforeMethod() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+		  System.out.println("afterMethod");
+		  driver.close();
+	  }
+
+}
